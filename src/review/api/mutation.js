abc762069fb56a58421fd310f454d03f6bf4532a
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-06-18 14:00:44 +0530

    review update and delete feature added

--- src/review/api/mutation.js
+++ src/review/api/mutation.js
@@ -1,11 +1,23 @@
-const { addReviewLogic } = require("../logic/logic");
+const {
+  addReviewLogic,
+  updateReviewLogic,
+  deleteReviewLogic,
+} = require("../logic/logic");
 
 const addReview = (parent, args, context, info) => {
   return addReviewLogic(parent, args, context, info);
 };
+const updateReview = (parent, args, context, info) => {
+  return updateReviewLogic(parent, args, context, info);
+};
+const deleteReview = (parent, args, context, info) => {
+  return deleteReviewLogic(parent, args, context, info);
+};
 
 const ReviewMutation = {
   addReview: addReview,
+  updateReview: updateReview,
+  deleteReview: deleteReview,
 };
 
 module.exports = { ReviewMutation };
--- src/review/data/data.js
+++ src/review/data/data.js
@@ -7,7 +7,7 @@ const addReviewData = async (newReview) => {
   const { productID, _id } = newReviewCreated;
 
   // adding referernce id  in  product schema
-  let updatedProduct = await Products.findOneAndUpdate(
+  await Products.findOneAndUpdate(
     { _id: productID },
     {
       $push: {
@@ -20,6 +20,24 @@ const addReviewData = async (newReview) => {
   );
   return newReviewCreated;
 };
-module.exports = { addReviewData };
 
-// when edit or delete review , we need to delete refernce also in product schema
+const updateReviewData = async (reviewID, updatedReview) => {
+  let updatedReviewData = await Review.findOneAndUpdate(
+    { _id: reviewID },
+    { $set: { ...updatedReview } },
+    { new: true }
+  );
+  return updatedReviewData;
+};
+
+// when  delete review , we need to delete refernce also in product schema
+// So we will need to have product id also
+const deleteReviewData = async (reviewID, productID) => {
+  const result = await Review.findByIdAndDelete(reviewID);
+  const productData = await Products.findOne({ _id: productID });
+  console.log(productData);
+
+  return result;
+};
+
+module.exports = { addReviewData, updateReviewData, deleteReviewData };
